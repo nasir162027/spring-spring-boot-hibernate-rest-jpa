1f4f9b07d5a589c7e8037c8a6e633e6a16dd9bc2
nasir <nasir@mislbd.com>
2024-04-01 00:45:56 +0600

    Profile & Address Update

--- src/main/java/com/naba/tech/controller/DashboardController.java
+++ src/main/java/com/naba/tech/controller/DashboardController.java
@@ -1,5 +1,8 @@
 package com.naba.tech.controller;
 
+import com.naba.tech.model.Person;
+import com.naba.tech.repository.PersonRepository;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -10,10 +13,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class DashboardController {
 
+    private final PersonRepository personRepository;
+
+    public DashboardController(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
     @RequestMapping("/dashboard")
-    public String displayDashboard(Model model,Authentication authentication) {
-        model.addAttribute("username", authentication.getName());
+    public String displayDashboard(Model model,Authentication authentication, HttpSession session) {
+        Person person = personRepository.readByEmail( getName( authentication ) );
+        model.addAttribute("username", person.getName());
         model.addAttribute("roles", authentication.getAuthorities().toString());
+        session.setAttribute("loggedInPerson", person);
         return "dashboard.html";
+    }
+
+    private static String getName(Authentication authentication) {
+        return authentication.getName();
     }
 }
--- src/main/java/com/naba/tech/model/Profile.java
+++ src/main/java/com/naba/tech/model/Profile.java
@@ -1,24 +1,25 @@
 package com.naba.tech.model;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.GenericGenerator;
 
 @Data
-@Entity
-public class Address extends BaseEntity{
+public class Profile {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
-    @GenericGenerator(name = "native",strategy = "native")
-    private int addressId;
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
+    private String name;
+
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{11})",message = "Mobile number must be 11 digits")
+    private String mobileNumber;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
+    private String email;
 
     @NotBlank(message="Address1 must not be blank")
     @Size(min=5, message="Address1 must be at least 5 characters long")
