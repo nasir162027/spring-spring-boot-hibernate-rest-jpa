3c13811538d0d9190297ad1ca05a2f8d76c06273
nasir <nasir@mislbd.com>
2024-04-18 21:47:56 +0600

    implement dynamic sorting

--- src/main/java/com/naba/tech/repository/CoursesRepository.java
+++ src/main/java/com/naba/tech/repository/CoursesRepository.java
@@ -4,6 +4,12 @@ import com.naba.tech.model.Courses;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CoursesRepository extends JpaRepository<Courses, Integer> {
+
+    List<Courses> findByOrderByNameDesc();
+
+    List<Courses> findByOrderByName();
 }
